50f13fd63c4b04de39f2d79cf813ca6b62c89d30
SandyCooper <112313667+SandyCooper@users.noreply.github.com>
2025-11-13 00:12:08 +0530

    Sorry Guys
    Replace the calculation with sorry guys

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ function App() {
     if (a>12){
       c = a-12;
     } else {
-      c = a;
+      c = "Sorry";
     }
     var minu = 0;
     if (str.length == 1){
       minu = "0"+str;
     } else {
-      minu = str;
+      minu = "Guys";
     }
     setHour(c);
     setMinute(minu);
