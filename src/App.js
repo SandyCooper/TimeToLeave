07615b76b6f2742e9f95d00bc64604a161bf5f13
SandyCooper <112313667+SandyCooper@users.noreply.github.com>
2025-11-14 13:00:48 +0530

    Back to form
    removed the sorry guys

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ function App() {
     if (a>12){
       c = a-12;
     } else {
-      c = "Sorry";
+      c = a;
     }
     var minu = 0;
     if (str.length == 1){
       minu = "0"+str;
     } else {
-      minu = "Guys";
+      minu = str;
     }
     setHour(c);
     setMinute(minu);
@@ -69,7 +69,7 @@ function App() {
         </div>
         <div className="click-button" onClick={handleClick}>CLICK</div>
       </div>
-      <div className={animate ? "white" : "white-prev"}>{"Sorry"}:{"Guys"}</div>
+      <div className={animate ? "white" : "white-prev"}>{hour}:{minute} PM</div>
     </div>
   );
 }
